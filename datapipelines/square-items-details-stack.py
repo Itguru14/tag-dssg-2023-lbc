746c802d598105b30b67a6e3f0ad0742fd9b5a20
Itguru14 <78121940+Itguru14@users.noreply.github.com>
2023-07-23 17:18:53 +0000

    Add files via upload

---
+++ datapipelines/square-items-details-stack.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import re
+import os
+import glob
+import numpy as np
+import pathlib
+from pathlib import Path
+#import Path
+
+# you must change cwd to where this file is located to run this filels
+
+flist = []
+for p in pathlib.Path('.').iterdir():
+    if p.is_file():
+        print(p)
+        flist.append(p)
+print(flist)
+
+df = pd.DataFrame()
+new_df=pd.DataFrame()
+for f in flist:
+
+     if  Path(f).suffix == '.xlsx':
+        new_df=pd.read_excel(f)
+        df= pd.concat([df,new_df])
+
+#print(df)
+     
+print(df.shape)
+
+deletion_threshhold=0.1                              #removes all columns in the dataframe with proportion of empty values greater than threshold
+df = df.loc[:, df.isin([' ','NULL', np.nan]).mean() < deletion_threshhold]    #may include whatever signify 'empty' 
+
+
+print(df.shape)
+
+df.to_csv('square-items-details-stack.csv')     
